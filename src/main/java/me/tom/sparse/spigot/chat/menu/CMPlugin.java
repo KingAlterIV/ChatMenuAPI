69845b11bb2ab7bf330e39618a57e8f2406a3ea7
MiniDigger <admin@minidigger.me>
2018-07-28 22:15:48 +0200

    delombok + formatting

--- src/main/java/me/tom/sparse/spigot/chat/menu/CMPlugin.java
+++ src/main/java/me/tom/sparse/spigot/chat/menu/CMPlugin.java
@@ -2,15 +2,12 @@ package me.tom.sparse.spigot.chat.menu;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CMPlugin extends JavaPlugin
-{
-	public void onEnable()
-	{
-		ChatMenuAPI.init(this);
-	}
-	
-	public void onDisable()
-	{
-		ChatMenuAPI.disable();
-	}
+public class CMPlugin extends JavaPlugin {
+    public void onEnable() {
+        ChatMenuAPI.init(this);
+    }
+
+    public void onDisable() {
+        ChatMenuAPI.disable();
+    }
 }
